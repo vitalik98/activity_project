3d9b17229fedba55bf1174d6b3f001f0a100ee4b
Vitali Mazanik <vmazanik@ibagroup.eu>
2022-04-11 19:53:48 +0300

    fixed configuration, removed duplicate dependency

--- src/main/kotlin/iba/by/activity_project/ActivityProjectApplication.kt
+++ src/main/kotlin/iba/by/activity_project/ActivityProjectApplication.kt
@@ -4,7 +4,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
-class ActivityProjectApplication
+open class ActivityProjectApplication
 
 fun main(args: Array<String>) {
 	runApplication<ActivityProjectApplication>(*args)
