de26e087ec33a1f196072d5831c29053ea9a3af5
Vitali Mazanik <vmazanik@ibagroup.eu>
2022-04-13 10:16:25 +0300

    fixed user resource

--- src/main/kotlin/iba/by/activity_project/resource/UserResource.kt
+++ src/main/kotlin/iba/by/activity_project/resource/UserResource.kt
@@ -3,7 +3,9 @@ package iba.by.activity_project.resource
 
 import iba.by.activity_project.entity.User
 import iba.by.activity_project.service.UserService
+import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
+import java.util.*
 
 @RestController
 @RequestMapping("/api/users")
@@ -12,9 +14,23 @@ class UserResource(private val userService: UserService) {
     @GetMapping
     fun index() = userService.getAllUsers()
 
+    @GetMapping("/{id}")
+    fun getUser(@PathVariable id: Long): ResponseEntity<Optional<User>> {
+        return if(userService.findUserById(id).isPresent)
+            ResponseEntity.ok(userService.findUserById(id))
+        else
+            ResponseEntity.notFound().build()
+    }
+
     @PostMapping
     fun post(@RequestBody user: User) = userService.post(user)
 
     @DeleteMapping("/{id}")
-    fun delete(@PathVariable id: Long) = userService.deleteUser(id)
+    fun delete(@PathVariable id: Long): ResponseEntity<Unit> {
+        return if(userService.findUserById(id).isPresent)
+            ResponseEntity.ok(userService.deleteUser(id))
+        else
+            ResponseEntity.badRequest().build()
+    }
+
 }
--- src/main/kotlin/iba/by/activity_project/service/UserService.kt
+++ src/main/kotlin/iba/by/activity_project/service/UserService.kt
@@ -3,6 +3,7 @@ package iba.by.activity_project.service
 import iba.by.activity_project.entity.User
 import iba.by.activity_project.repository.UserRepository
 import org.springframework.stereotype.Service
+import java.util.*
 
 @Service
 class UserService(private val db: UserRepository) {
@@ -11,6 +12,9 @@ class UserService(private val db: UserRepository) {
 
     fun post(user: User) = db.save(user)
 
+
     fun deleteUser(id: Long) = db.deleteById(id)
+
+    fun findUserById(id: Long): Optional<User> = db.findById(id)
 
 }
